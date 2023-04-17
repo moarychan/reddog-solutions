8c919033a9f5b1c6dbf96d226ab6592b7a99c5a1
Shili Chen <v-shilichen@microsoft.com>
2023-04-17 08:40:24 +0800

    test order

--- order-service/src/main/java/com/microsoft/gbb/reddog/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microsoft/gbb/reddog/orderservice/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.microsoft.gbb.reddog.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
 @EnableJms
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
